4a19936a871b91246cf07bfbde058fa77dd61787
michaelmarytan81194 <133595613+michaelmarytan81194@users.noreply.github.com>
2023-09-04 01:31:37 +0700

    Update 1

---
+++ Storing, Retrieving, and Processing JSON data with Python/Character_Module.py
@@ -0,0 +1,11 @@
+class Character:
+    def __init__(sefl, name, gender, species, origin, status, image, number_of_episodes):
+        self.name = name
+        self.gender = gender
+        self.species = species
+        self.origin = origin
+        self.status = status
+        self.image_url = image
+        self.number_episodes = number_of_episodes
+    def show_character(self):
+        print(self.name, self.gender)
